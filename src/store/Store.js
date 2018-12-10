7e2c3dace67bb114395b5401b76b036507378fe9
“chenzz <l>
2018-12-10 16:41:33 +0800

    git add .

--- src/store/Store.js
+++ src/store/Store.js
@@ -3,15 +3,35 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '北京'
+let defaultId = 1
+try {
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+    defaultId = localStorage.id
+  }
+} catch (e) {
+  console.log("在本地存储中没");
+}
+
 export default new Vuex.Store({
+
   state: {
-      city: '北京',
-      id: '1'
+    city: defaultCity,
+    id: defaultId
   },
   mutations: {
-    changeCity (state,city) {
+
+    changeCity(state, city) {
       state.city = city.name
       state.id = city.id
+      try {
+        localStorage.city = city.name
+        localStorage.id = city.id
+      } catch (e) {
+        console.log("在本地存储中没");
+
+      }
     }
 
   }
